a4f5637c710d3ef2d81ac07ec58436caf2ec3615
Amit Yadav <iamamityadav1020@gmail.com>
2025-12-05 17:27:58 +0530

    Todo App Final

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,19 @@
 from django.urls import path 
 from . import views
 urlpatterns = [
+    # Add Task Feature
     path('addTask/',views.addTask, name='addTask'),
+
+    # Mark as done feature
+    path('mark_as_done/<int:pk>/',views.mark_as_done, name='mark_as_done'),
+
+    # Mark as Undone Feature
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone, name='mark_as_undone'),
+
+    # Edit Feature
+    path('edit_task/<int:pk>/',views.edit_task, name='edit_task'),
+
+    #Delete Feature
+    path('delete_task/<int:pk>/',views.delete_task, name='delete_task'),
+
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(isCompleted=False).order_by('-updated_at')  #'-' is used for we get the data in descending order, by defult it is ascending order
+
+    completed_task = Task.objects.filter(isCompleted=True).order_by('-updated_at')
+    # print(completed_task)
     # print(tasks)
     context = {
         'tasks': tasks,
+        'completed_task' : completed_task,
     }
     return render(request, 'home.html',context)
